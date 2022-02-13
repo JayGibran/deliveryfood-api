bc0ce421c61ed1eadd25453d689ab1e396edfc05
jaygibran <jay.gibran@sonalake.com>
2022-02-13 21:08:49 +0000

    implementing simple query with criteria api

--- src/main/java/com/jaygibran/deliveryfood/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/jaygibran/deliveryfood/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -4,13 +4,13 @@ import com.jaygibran.deliveryfood.domain.model.Restaurant;
 import com.jaygibran.deliveryfood.domain.repository.RestaurantRepositoryQueries;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
-import java.util.HashMap;
 import java.util.List;
 
 @AllArgsConstructor
@@ -23,30 +23,12 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     @Override
     public List<Restaurant> find(String name, BigDecimal feeDeliveryMin, BigDecimal feeDeliveryMax) {
 
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurant where 0 = 0");
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
 
-        var parameters = new HashMap<String, Object>();
+        CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
+        criteria.from(Restaurant.class);
 
-        if (StringUtils.hasLength(name)) {
-            jpql.append("and name like :name ");
-            parameters.put("name", "%" + name + "%");
-        }
-
-        if (feeDeliveryMin != null) {
-            jpql.append("and feeDelivery >= :feeDeliveryMin ");
-            parameters.put("feeDeliveryMin", feeDeliveryMin);
-        }
-
-        if (feeDeliveryMax != null) {
-            jpql.append("and feeDelivery <= :feeDeliveryMax");
-            parameters.put("feeDeliveryMax", feeDeliveryMax);
-        }
-
-        TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
-
-        parameters.forEach((key, value) -> query.setParameter(key, value));
-
+        TypedQuery<Restaurant> query = entityManager.createQuery(criteria);
         return query.getResultList();
     }
 }
