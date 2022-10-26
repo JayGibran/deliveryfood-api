c9221c253160d4015d128a0df3f70b30994980df
jaygibran <jay.gibran@sonalake.com>
2022-10-26 21:58:02 +0100

    Adding some information on documentation api

--- src/main/java/com/jaygibran/deliveryfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/jaygibran/deliveryfood/core/openapi/SpringFoxConfig.java
@@ -3,8 +3,11 @@ package com.jaygibran.deliveryfood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +22,15 @@ public class SpringFoxConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.jaygibran.deliveryfood.api"))
                 .paths(PathSelectors.any())
 //                .paths(PathSelectors.ant("/restaurants/*"))
+                .build().apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("DeliveryFood API")
+                .description("Open Api for clients and restaurants")
+                .version("1")
+                .contact(new Contact("DeliveryFood", "https://www.deliveryfood.com", "contato@deliveryfood.com"))
                 .build();
     }
 }
