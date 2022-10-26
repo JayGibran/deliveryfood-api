7ed4f2549a0e0a9ab0bee854abe3dc7e346111c2
jaygibran <jay.gibran@sonalake.com>
2022-10-26 20:55:37 +0100

    Defining endpoints which will generate documentation

--- src/main/java/com/jaygibran/deliveryfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/jaygibran/deliveryfood/core/openapi/SpringFoxConfig.java
@@ -3,6 +3,7 @@ package com.jaygibran.deliveryfood.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,7 +16,9 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.jaygibran.deliveryfood.api"))
+                .paths(PathSelectors.any())
+//                .paths(PathSelectors.ant("/restaurants/*"))
                 .build();
     }
 }
