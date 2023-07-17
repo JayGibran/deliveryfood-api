95dad65f6b8043549fab35ffe9b2cffb89ffdb4a
jaygibran <jay.gibran@sonalake.com>
2023-07-17 21:20:06 +0100

    Storing authorization in the database

--- src/main/java/com/jaygibran/deliveryfood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/jaygibran/deliveryfood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -1,24 +1,20 @@
 package com.jaygibran.deliveryfood.core.security.authorizationserver;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.OAuth2TokenFormat;
-import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
-import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
-import org.springframework.security.web.SecurityFilterChain;
+import java.time.*;
+import java.util.*;
 
-import java.time.Duration;
-import java.util.Arrays;
+import org.springframework.context.annotation.*;
+import org.springframework.core.*;
+import org.springframework.core.annotation.*;
+import org.springframework.jdbc.core.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.oauth2.core.*;
+import org.springframework.security.oauth2.server.authorization.*;
+import org.springframework.security.oauth2.server.authorization.client.*;
+import org.springframework.security.oauth2.server.authorization.config.*;
+import org.springframework.security.web.*;
 
 @Configuration
 public class AuthorizationServerConfig {
@@ -56,5 +52,11 @@ public class AuthorizationServerConfig {
                 .build();
 
         return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodbackend));
+    }
+    
+    @Bean
+    public OAuth2AuthorizationService oAuth2AuthorizationService(JdbcOperations jdbcOperations, 
+                                                                 RegisteredClientRepository registeredClientRepository){
+        return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
     }
 }
